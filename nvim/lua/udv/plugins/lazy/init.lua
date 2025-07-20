0997b9833e665aa9507566a84677495ad8e19e9f
raydrivers <dmitry.vasyliev@protonmail.com>
2025-07-20 12:47:13 +0300

    Add Hyprland and kitty configuration

--- nvim/lua/udv/plugins/lazy/init.lua
+++ nvim/lua/udv/plugins/lazy/init.lua
@@ -120,6 +120,7 @@ end
 
 local themes = themes_from_directory(THEMES_CONFIG_DIR)
 table.insert(themes, { "ramojus/mellifluous.nvim", lazy = false })
+table.insert(themes, { "RRethy/base16-nvim", lazy = false })
 
 add_plugin {
     "rktjmp/lush.nvim",
