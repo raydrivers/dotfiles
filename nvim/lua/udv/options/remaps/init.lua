e61822b3e7839abef9fb0eb25174f6dc2895eab2
raydrivers <dmitry.vasyliev@protonmail.com>
2025-07-27 20:29:55 +0300

    Clean up neovim plugins

--- nvim/lua/udv/options/remaps/init.lua
+++ nvim/lua/udv/options/remaps/init.lua
@@ -1,10 +1,11 @@
 require("udv.options.remaps.global")
-require("udv.options.remaps.vscode")
 
 if not vim.g.vscode then
     require("udv.options.remaps.telescope")
     require("udv.options.remaps.windows")
     require("udv.options.remaps.oil")
     require("udv.options.remaps.neotree")
+else
+    require("udv.options.remaps.vscode")
 end
 
