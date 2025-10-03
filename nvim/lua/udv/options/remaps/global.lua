ed09b206d7eaa41cc6227370d3079d69745f2706
Dmitry Vasyliev <dmitry.vasyliev@protonmail.com>
2025-10-03 17:37:38 +0100

    Minor fixes and corrections

--- nvim/lua/udv/options/remaps/global.lua
+++ nvim/lua/udv/options/remaps/global.lua
@@ -1,1 +1,6 @@
 local map = require("udv.utils.keymap")
+
+-- Buffer navigation (missing from config)
+vim.keymap.set("n", "]b", ":bnext<CR>", { desc = "Next buffer" })
+vim.keymap.set("n", "[b", ":bprev<CR>", { desc = "Previous buffer" })
+vim.keymap.set("n", "<leader>bd", ":bdelete<CR>", { desc = "Delete buffer" })
--- nvim/lua/udv/plugins/config/lspconfig/init.lua
+++ nvim/lua/udv/plugins/config/lspconfig/init.lua
@@ -5,6 +5,11 @@ local function on_attach(client, bufnr)
         vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
     end
 
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
     vim.keymap.set({ "n", "i" }, "<c-p>", vim.lsp.buf.signature_help, opts)
     vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
     vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
