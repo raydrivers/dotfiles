e206b426c5107633a814933d524392749de3eea1
raydrivers <dmitry.vasyliev@protonmail.com>
2025-12-15 17:58:11 +0000

    Add neovim minimum version check

--- nvim/lua/udv/prelude/init.lua
+++ nvim/lua/udv/prelude/init.lua
@@ -1,3 +1,9 @@
+-- Version check - require Neovim 0.10+
+if vim.fn.has("nvim-0.10") == 0 then
+  vim.notify("Neovim 0.10+ required. Current: " .. vim.version().minor, vim.log.levels.ERROR)
+  return
+end
+
 require("udv.prelude.vimrc")
 require("udv.prelude.globals")
 require("udv.prelude.util")
